2a4e687e330c79a27027aea800ef159298133c7e
Josias Alvarado <josias@pulpowms.com>
2022-08-23 22:13:48 -0600

    Add initial working simple spider to crawl SCP data

---
+++ lib/spider.ex
@@ -0,0 +1,29 @@
+defmodule Millar.Spider do
+  @base_url "https://scp-wiki.wikidot.com/scp-{number}"
+
+  def get_scp(number) do
+    case do_get_scp(number) do
+      :get_scp_error -> raise("Unable to retrieve SCP-#{number}.")
+      response -> parse_scp_response(response)
+    end
+  end
+
+  defp parse_scp_response(%HTTPoison.Response{status_code: 200, body: body}) do
+    %{
+      title: Floki.find(body, "#page-title"),
+      content: Floki.find(body, "#page-content")
+    }
+  end
+
+  defp do_get_scp(scp_number) do
+    url = Regex.replace(~r/\{number\}/, @base_url, scp_number)
+
+    case HTTPoison.get(url) do
+      {:ok, response} ->
+        response
+
+      {:error, _} ->
+        :get_scp_error
+    end
+  end
+end
